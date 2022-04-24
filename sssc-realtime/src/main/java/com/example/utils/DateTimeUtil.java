3f11dd6b73f1b6af26c8321d773b28938f43b096
MrGOTHAM <805595674@qq.com>
2022-04-24 13:20:19 +0800

    .

--- sssc-realtime/src/main/java/com/example/utils/DateTimeUtil.java
+++ sssc-realtime/src/main/java/com/example/utils/DateTimeUtil.java
@@ -15,16 +15,16 @@ import java.util.Date;
  */
 public class DateTimeUtil {
 
-    private final static DateTimeFormatter formater =
+    private final static DateTimeFormatter formatter =
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     public static String toYMDhms(Date date) {
         LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(),
                 ZoneId.systemDefault());
-        return formater.format(localDateTime);
+        return formatter.format(localDateTime);
     }
     public static Long toTs(String YmDHms) {
         // 年月日+时分秒
-        LocalDateTime localDateTime = LocalDateTime.parse(YmDHms, formater);
+        LocalDateTime localDateTime = LocalDateTime.parse(YmDHms, formatter);
 
         return localDateTime.toInstant(ZoneOffset.of("+8")).toEpochMilli();
     }
